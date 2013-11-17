da109a03829dd01b6dfbf9eec3ea17dedc6d857d
Ron Sharpe <hesychast85@gmail.com>
2013-11-17 16:23:18 -0500

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,11 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
+		#render 'visitors/new'..hidden render method combines (or inserts) the app/views/visitors/new/html.erb view file
+		# with (or into) the app/views/layouts/application.html.erb: applicaton layout
+		flash.now[:notice] = 'Welcome!'
+		flash.now[:alert] = 'My birthday is soon.'
+
 	end
 
 end

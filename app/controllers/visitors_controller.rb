7a0b610ab4f9394dab0b25d0f3bc14fb7c26918a
Ron Sharpe <hesychast85@gmail.com>
2013-11-18 00:56:42 -0500

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,8 +4,7 @@ class VisitorsController < ApplicationController
 		@owner = Owner.new
 		#render 'visitors/new'..hidden render method combines (or inserts) the app/views/visitors/new/html.erb view file
 		# with (or into) the app/views/layouts/application.html.erb: applicaton layout
-		flash.now[:notice] = 'Welcome!'
-		flash.now[:alert] = 'My birthday is soon.'
+		
 
 	end
 

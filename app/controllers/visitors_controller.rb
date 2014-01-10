97ddbe8a0d534fbf3d7d936fe97b4e6ce2d21cf9
Ron Sharpe <hesychast85@gmail.com>
2014-01-09 19:23:32 -0500

    mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,11 +1,20 @@
 class VisitorsController < ApplicationController
-
-	def new
-		@owner = Owner.new
-		#render 'visitors/new'..hidden render method combines (or inserts) the app/views/visitors/new/html.erb view file
-		# with (or into) the app/views/layouts/application.html.erb: applicaton layout
-		
-
-	end
-
+def new
+@visitor = Visitor.new
 end
+def create
+@visitor = Visitor.new(secure_params)
+if @visitor.valid?
+@visitor.subscribe
+flash[:notice] = "Signed up #{@visitor.email}."
+redirect_to root_path
+else
+render :new
+end
+end
+private
+def secure_params
+params.require(:visitor).permit(:email)
+end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 
 LearnRails::Application.routes.draw do
-	resources :contacts, only: [:new, :create]
-root to: 'visitors#new' 
+resources :contacts, only: [:new, :create]
+resources :visitors, only: [:new, :create]
+root to: 'visitors#new'
 end
+
 
